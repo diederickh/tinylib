b72fded5b14999c29bd00afc0b9695bdb5e80512
roxlu <diederick@apollomedia.nl>
2014-08-27 10:05:55 +0200

    added rx_string_id()

--- src/tinylib.h
+++ src/tinylib.h
@@ -3018,6 +3018,17 @@ extern int rx_get_minute() {
   return rx_to_int(rx_strftime("%M"));
 }
 
+#undef get16bits
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+|| defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define get16bits(d) (*((const uint16_t *) (d)))
+#endif
+
+#if !defined (get16bits)
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
++(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+
 extern uint32_t rx_string_id(const std::string& v) {
   const char* data = v.c_str();
   int len = v.size();
